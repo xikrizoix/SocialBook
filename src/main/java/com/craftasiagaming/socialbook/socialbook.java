e549db9646e905a4b82786703a7dbff0910a0ac2
unknown <Nick@Nickate-PC.(none)>
2012-04-15 10:50:02 +1000

    Fixed package.

--- src/main/java/com/craftasiagaming/socialbook/socialbook.java
+++ src/main/java/com/craftasiagaming/socialbook/socialbook.java
@@ -1,14 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.craftasiagaming.socialbook;
 
-/**
- *
- * @author Nick
- */
 public class socialbook {
     
     //xikrizoix was here.
+    // *trollface* ~ yoshi
     
 }
